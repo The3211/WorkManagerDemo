1ca05c9abb31c846dacd1bf31908d494ce17c77e
The3211 <aiyaz.parmar@gmail.com>
2022-03-15 20:14:59 +0530

    Added workmanager dependency - Added OneTimeWorkRequest - Added UploadWorker.kt file for workmanager

--- app/src/main/java/com/aiyaz/workmanagerdemo/MainActivity.kt
+++ app/src/main/java/com/aiyaz/workmanagerdemo/MainActivity.kt
@@ -2,10 +2,33 @@ package com.aiyaz.workmanagerdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
 
 class MainActivity : AppCompatActivity() {
+
+
+    lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.button)
+        textView = findViewById(R.id.textView)
+        button.setOnClickListener {
+            setOneTimeWorkRequest()
+        }
+    }
+
+    private fun setOneTimeWorkRequest() {
+        val workManager = WorkManager.getInstance(applicationContext)
+        val uploadRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java).build()
+        workManager.enqueue(uploadRequest)
+        workManager.getWorkInfoByIdLiveData(uploadRequest.id).observe(this, Observer {
+            textView.text = it.state.name
+        })
     }
 }
