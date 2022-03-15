6276f15f0e83d7aa05f390b230007df8299dd4c6
The3211 <aiyaz.parmar@gmail.com>
2022-03-16 00:59:17 +0530

    - Constraints added in work manager   Requires Charging   NetworkType Connected.

--- app/src/main/java/com/aiyaz/workmanagerdemo/MainActivity.kt
+++ app/src/main/java/com/aiyaz/workmanagerdemo/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.lifecycle.Observer
+import androidx.work.Constraints
+import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequest
 import androidx.work.WorkManager
 
@@ -25,7 +27,15 @@ class MainActivity : AppCompatActivity() {
 
     private fun setOneTimeWorkRequest() {
         val workManager = WorkManager.getInstance(applicationContext)
-        val uploadRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java).build()
+        val constraints = Constraints.Builder()
+            .setRequiresCharging(true)
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+
+        val uploadRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java)
+                .setConstraints(constraints)
+                .build()
+
         workManager.enqueue(uploadRequest)
         workManager.getWorkInfoByIdLiveData(uploadRequest.id).observe(this, Observer {
             textView.text = it.state.name
